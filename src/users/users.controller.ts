8c0ef7cbbb53a799f1704d044a38e7f26217aa32
afmireski <afmireski@outlook.com>
2021-12-28 15:05:42 -0300

    - Create findUserById and findUserbyEmail end-points.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get(':id/id')
+  findUserById(@Param('id') id: string): Promise<User> {
+    return this.usersService.findUnique({ id: id });
+  }
+
+  @Get(':email/email')
+  findUserByEmail(@Param('email') email: string): Promise<User> {
+    return this.usersService.findUnique({ email: email });
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { User, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findUnique(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return await this.prisma.user.findUnique({
+      where: where,
+    });
+  }
+
+}
