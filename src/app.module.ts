589dc5663fb36ac8eb100288ca584f348fff4ece
afmireski <afmireski@outlook.com>
2021-12-28 09:16:04 -0300

    - Create 'users' resources.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'nestjs-prisma';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     PrismaModule.forRoot({
       isGlobal: true,
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
