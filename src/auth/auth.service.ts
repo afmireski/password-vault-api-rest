8689b3e47097b3b25b098a2d8e9828b95adeb05d
afmireski <afmireski@outlook.com>
2021-12-29 21:53:34 -0300

    - Create AuthStrategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private prismaService: PrismaService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<User> {
+    return Promise.resolve(this.usersService.findUnique({ email: email }))
+      .then((user) => {
+        if (!user && bcrypt.compareSync(password, user.password)) {
+          return null;
+        }
+        return user;
+      })
+      .then();
+  }
+}
