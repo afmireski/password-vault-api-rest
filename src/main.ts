9a085eca8b201d4713e73b4717a969faa7e6711f
afmireski <afmireski@outlook.com>
2022-01-03 09:34:10 -0300

    - Implement SwaggerModule setup.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('password-vault/api-rest');
+
+  const docConfig = new DocumentBuilder()
+    .setTitle('PasswordVault API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, docConfig);
+  SwaggerModule.setup('password-vault/api-rest/api', app, document);
 
   const configService = app.get(ConfigService);
 
