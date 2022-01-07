47210a3fbb48a9d0ac7f74554a8988ca22cbe8bc
afmireski <afmireski@outlook.com>
2022-01-07 10:38:03 -0300

    - UserService reworked.

--- src/error-codes/100-user-errors.ts
+++ src/error-codes/100-user-errors.ts
@@ -1,12 +1,12 @@
 const userErrors = {
-  100: 'A user with this e-mail already exists',
-  101: "A user with this id doesn't exists",
+  101: "A user with this credential doesn't exists",
   102: "'new_password' value must be different than 'current_password' value",
   103: "'new_password' value must be equals than 'confirm_new_password' value",
   104: "The informed 'current_password' value isn't equals as the 'current user password' value",
   105: "The informed password isn't equals as this user password",
   106: "A user with this e-mail doesn' t exists",
   107: "This user credentials aren't valid",
+  108: 'A user with this e-mail already exists',
 };
 
 export { userErrors };
